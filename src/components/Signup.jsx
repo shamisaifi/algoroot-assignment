8c54397861564cc00a7f27724eff0500b7e120cb
shamisaifi <shamisaifi2003.com>
2025-03-26 20:10:23 +0530

    change

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -92,9 +92,12 @@ const Signup = () => {
         </form>
         <p className="text-sm text-center text-gray-600">
           Already have an account?{" "}
-          <a href="/login" className="text-blue-500 hover:underline">
+          <span
+            onClick={() => navigate("/login")}
+            className="text-blue-500 hover:underline"
+          >
             Login
-          </a>
+          </span>
         </p>
       </div>
     </div>
